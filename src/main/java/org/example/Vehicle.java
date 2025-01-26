704d9ae87a1b334192f0abffbf3f2ddc753700e8
Dxcraig <steviedoe20@gmail.com>
2025-01-26 07:52:05 +0000

    Added constructor to initialize private fields

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -5,4 +5,12 @@ public abstract class Vehicle {
     private String model;
     private double fuelLevel;
 
+    public Vehicle(String vehicleId, String model, double fuelLevel) {
+        this.vehicleId = vehicleId;
+        this.model = model;
+        this.fuelLevel = fuelLevel;
+    }
+
+
+
 }
