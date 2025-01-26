25b12d1d53acb8e3636d4ad522790da4bd6c55ec
Dxcraig <steviedoe20@gmail.com>
2025-01-26 08:00:12 +0000

    Implemented parent class methods

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,4 +1,13 @@
 package org.example;
 
-public class Car {
+public class Car extends Vehicle{
+
+    public Car(String vehicleId, String model, double fuelLevel) {
+        super(vehicleId, model, fuelLevel);
+    }
+
+    @Override
+    public void calculateRange() {
+
+    }
 }
